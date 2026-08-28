38f9f5bf1495fabbf42e257e1838c9fd11456c5b
pinkyhadgehog <oksana.kholodiuk@gmail.com>
2026-08-28 12:15:12 +0100

    Configure Flask for Codio preview

--- app.py
+++ app.py
@@ -281,4 +281,8 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(
+        host="0.0.0.0",
+        debug=True,
+        port=5002
+    )
